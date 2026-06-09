32b614c40cc3eb2caa2dbed4812b44eaf6760b0a
Mukesh Bhatt <mukesh.bhatt@qlik.com>
2026-06-09 11:02:43 +0530

    added improvements for file straming (#7)
    * added improvements for file streaming

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+  * Improve file streaming [#07](https://github.com/singer-io/tap-azure-cloud-storage/pull/7)
+  * Bump dependencies for compliance
+
 ## 0.0.1
 
   * Initial release [#1](https://github.com/singer-io/tap-azure-cloud-storage/pull/1)
--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 from setuptools import setup
 
 setup(name='tap-azure-cloud-storage',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer tap for extracting files from Azure Cloud Storage',
       author='Qlik',
       url='https://www.qlik.com',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       install_requires=[
           'backoff==2.2.1',
-          'urllib3==2.6.3',
+          'urllib3==2.7.0',
           'singer-encodings==0.5.0',
           'singer-python==6.8.0',
-          'azure-storage-blob==12.28.0',
+          'azure-storage-blob==12.29.0',
           'azure-identity==1.25.3',
-          'adlfs==2026.2.0',
+          'adlfs==2026.5.0',
           'voluptuous==0.16.0'
       ],
       extras_require={
